380e6e0bab6b3ed8cc4a5c2b5ab4cfcba3f77231
Diego Osvaldo Agudo <dagudo@petersen.corp>
2023-06-27 15:10:00 -0300

    Agregado CORS

--- src/main/java/com/agudodiego/quePague/config/SecurityConfig.java
+++ src/main/java/com/agudodiego/quePague/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,6 +30,7 @@ public class SecurityConfig {
         http
             .csrf()
             .disable()
+            .cors(Customizer.withDefaults())
 
             // configuramos la "lista blanca" (aquellas urls que no necesitan filtrarse)
             .authorizeHttpRequests()
